cda5c6d21e4ac5382a9b0b3f4450119bd10562f7
yuerain <yuerainiloveyou@163.com>
2020-03-27 00:09:27 +0800

    yuerain
    ex10  绘图

--- sample/【第十节】PyQt5绘图/example_02.py
+++ sample/【第十节】PyQt5绘图/example_02.py
@@ -1,63 +1,47 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 
-# 拖放一个按钮
+# 画点
 
-
+import random
 import sys
-from PyQt5.QtWidgets import QPushButton, QWidget, QApplication
-from PyQt5.QtCore import Qt, QMimeData
-from PyQt5.QtGui import QDrag
 
-class Button(QPushButton):
-    def __init__(self, title, parent):
-        super().__init__(title, parent)
-
-    def mouseMoveEvent(self, e):
-        if e.buttons() != Qt.RightButton:
-            return
-
-        mimeData = QMimeData()
-
-        drag = QDrag(self)
-        drag.setMimeData(mimeData)
-        drag.setHotSpot(e.pos() - self.rect().topLeft())
-
-        dragAction = drag.exec_(Qt.MoveAction)
-
-    def mousePressEvent(self, e):
-        QPushButton.mousePressEvent(self, e)
-        if e.button() == Qt.LeftButton:
-            print('press')
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import QWidget, QApplication
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.initUI();
 
     def initUI(self):
-        self.setAcceptDrops(True)
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('Points')
+        self.show()
 
-        self.button = Button('Button', self)
-        self.button.move(100, 65)
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawPoints(qp)
+        qp.end()
 
-        self.setWindowTitle('Click or Move')
-        self.setGeometry(300, 300, 280, 150)
+    def drawPoints(self, qp):
+        qp.setPen(Qt.red)
+        size = self.size()
 
-    def dragEnterEvent(self, e):
-        e.accept()
+        cColor = [Qt.red, Qt.green, Qt.blue]
 
-    def dropEvent(self, e):
-        position = e.pos()
-        self.button.move(position)
-
-        e.setDropAction(Qt.MoveAction)
-        e.accept()
+        for i in range(1000):
+            qp.setPen(random.choice(cColor))
+            # print(random.randint(0, 2))
+            x = random.randint(1, size.width() - 1)
+            y = random.randint(1, size.height() - 1)
+            qp.drawPoint(x, y)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
-    ex.show()
     sys.exit(app.exec_())
